4649235556dbde387065b049d836a02a3bcd8745
onewaleed <98025193+onewaleed@users.noreply.github.com>
2022-02-19 04:41:19 +0000

    Updated Action
    Added View to Action

--- src/main/java/com/qa/ims/controller/Action.java
+++ src/main/java/com/qa/ims/controller/Action.java
@@ -13,7 +13,8 @@ import com.qa.ims.utils.Utils;
 public enum Action {
 	CREATE("To save a new entity into the database"), READ("To read an entity from the database"),
 	UPDATE("To change an entity already in the database"), DELETE("To remove an entity from the database"),
-	RETURN("To return to domain selection");
+	RETURN("To return to domain selection"), 
+	VIEW("To view all entities");// added view to action commands
 
 	public static final Logger LOGGER = LogManager.getLogger();
 
