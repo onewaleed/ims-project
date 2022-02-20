4e4ecbaf1e6a8bc671ab78c7457978b4089d0854
onewaleed <98025193+onewaleed@users.noreply.github.com>
2022-02-20 04:32:57 +0000

    Final ItemController
    Updated to allow functions of;
    Create order
    View all orders
    Delete an order
    Add an item to an order
    Calculate the cost for an order
    Delete a item in an order.

--- src/main/java/com/qa/ims/controller/ItemController.java
+++ src/main/java/com/qa/ims/controller/ItemController.java
@@ -80,4 +80,10 @@ public class ItemController implements CrudController<Item> {
 		return itemDAO.delete(id);
 	}
 
+	@Override
+	public void calculate() {
+		// TODO Auto-generated method stub
+		
+	}
+
 }
